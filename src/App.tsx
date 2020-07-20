375f52cfb2888069d78c588bef50e3f71a946178
rafaelreisramos <rrramos@gmail.com>
2020-07-20 15:13:06 -0300

    global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const App: React.FC = () => <h1>GoBarber Web</h1>;
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <h1>GoBarber Web</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
