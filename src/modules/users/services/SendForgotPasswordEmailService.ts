cbfa57f96618cca0471b35210161c22e2db4df56
rafaelreisramos <rrramos@gmail.com>
2020-08-25 13:07:29 -0300

    Created a fake mail provider fot tests. Basic send e-mail test.

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,25 @@
+import { inject, injectable } from 'tsyringe';
+
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import IUsersRepository from '../repositories/IUsersRepository';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    this.mailProvider.sendMail(email, 'Recovery password request received');
+  }
+}
+
+export default SendForgotPasswordEmailService;
