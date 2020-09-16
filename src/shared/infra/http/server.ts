08488c6487cb11fd9ce4a5f8a8c7e0be0783bf66
rafaelreisramos <rrramos@gmail.com>
2020-09-16 16:16:58 -0300

    Server started message log->info.

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -35,5 +35,5 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
 });
 
 app.listen(3333, () => {
-  console.log('Server started on port 3333');
+  console.info('Server started on port 3333');
 });
