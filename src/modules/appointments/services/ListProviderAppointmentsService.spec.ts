0810d3c017060549d4aac4b9058d04d2ca7b619e
rafaelreisramos <rrramos@gmail.com>
2020-09-29 09:46:44 -0300

    Provider daily appointments list service, controller and route.

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,37 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderAppointments: ListProviderAppointmentsService;
+
+describe('ListProviderAppointments', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderAppointments = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list daily appointments', async () => {
+    const appointment1 = await fakeAppointmentsRepository.create({
+      date: new Date(2020, 8, 29, 13),
+      provider_id: 'provider_id',
+      user_id: 'user_id',
+    });
+
+    const appointment2 = await fakeAppointmentsRepository.create({
+      date: new Date(2020, 8, 29, 14),
+      provider_id: 'provider_id',
+      user_id: 'user_id',
+    });
+
+    const appointments = await listProviderAppointments.execute({
+      provider_id: 'provider_id',
+      day: 29,
+      month: 9,
+      year: 2020,
+    });
+
+    expect(appointments).toEqual([appointment1, appointment2]);
+  });
+});
