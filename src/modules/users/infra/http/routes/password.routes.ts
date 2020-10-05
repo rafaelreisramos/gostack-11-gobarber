0e7ec8f2fea13f682d94424b492b09e97711cb98
rafaelreisramos <rrramos@gmail.com>
2020-10-05 11:09:03 -0300

    Validation with celebrate.

--- src/modules/users/infra/http/routes/password.routes.ts
+++ src/modules/users/infra/http/routes/password.routes.ts
@@ -1,11 +1,30 @@
 import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 import PasswordController from '../controllers/PasswordController';
 
 const passwordRouter = Router();
 const passwordController = new PasswordController();
 
-passwordRouter.post('/', passwordController.create);
-passwordRouter.post('/reset', passwordController.update);
+passwordRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+    },
+  }),
+  passwordController.create,
+);
+passwordRouter.post(
+  '/reset',
+  celebrate({
+    [Segments.BODY]: {
+      token: Joi.string().uuid().required(),
+      password: Joi.string().required(),
+      password_confirmation: Joi.string().required().valid(Joi.ref('password')),
+    },
+  }),
+  passwordController.update,
+);
 
 export default passwordRouter;
