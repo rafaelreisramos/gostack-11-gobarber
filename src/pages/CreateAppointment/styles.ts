da544abe29c2061776dd1576d22e6b0ad53308ad
rafaelreisramos <rrramos@gmail.com>
2020-10-23 14:52:14 -0300

    Fix: Android header padding top.

--- src/pages/CreateAppointment/styles.ts
+++ src/pages/CreateAppointment/styles.ts
@@ -18,7 +18,7 @@ export const Container = styled.View`
 
 export const Header = styled.View`
   padding: 24px;
-  /* padding-top: ${getStatusBarHeight() + 24}px; */
+  padding-top: ${getStatusBarHeight() + 24}px;
   background: #28262e;
 
   flex-direction: row;
