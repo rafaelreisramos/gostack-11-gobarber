bf0498801b45cea39c4b7e1a281762a84cf0f1b2
rafaelreisramos <rrramos@gmail.com>
2022-05-25 17:48:20 -0300

    test: add signin render page test

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -5,7 +5,7 @@ import React, {
   useEffect,
   useState,
 } from 'react';
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import api from '../services/api';
 
