3e63ad6571d08340d35b10fdf3f07ad479343039
rafaelreisramos <rrramos@gmail.com>
2022-05-27 14:03:16 -0300

    feat: add env variables and docker compose

---
+++ api/ormconfig.ts
@@ -0,0 +1,27 @@
+import 'dotenv/config';
+
+export = [
+  {
+    name: 'default',
+    type: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASS,
+    entities: ['./src/modules/**/infra/typeorm/entities/*.ts'],
+    migrations: ['./src/shared/infra/typeorm/migrations/*.ts'],
+    cli: {
+      migrationsDir: './src/shared/infra/typeorm/migrations',
+    },
+  },
+  {
+    name: 'mongo',
+    type: 'mongodb',
+    host: process.env.MONGODB_HOST,
+    port: process.env.MONGODB_PORT,
+    database: process.env.MONGODB_DB,
+    useUnifiedTopology: true,
+    entities: ['./src/modules/**/infra/typeorm/schemas/*.ts'],
+  },
+];
